71bbc1c9e49066f15afbaee1e56e2b0306a1da49
Revaliza123 <116142603+Revaliza123@users.noreply.github.com>
2024-02-15 07:26:58 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 import React from "react";
-import { createBrowserRouter, Route, Routes } from "react-router-dom";
-
-import Home from "./pages/Home.jsx";
-import Sigin from "./pages/sigin.jsx";
-import About from "./pages/about.jsx";
-import Dashboard from "./pages/dashboard.jsx";
-import Dsource from "./pages/dsource.jsx";
+import orang from './assets/gambar1.png';
+import logo from './assets/Vector.png';
+import Sigin from './pages/sigin.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/about.jsx';
+import Dashboard from './pages/dashboard.jsx';
+import Dsource from './pages/dsource.jsx';
+import Masuk from './components/masuk.jsx';
+import { BrowserRouter as  Router, Routes, Route } from "react-router-dom";
 import Notfound from "./pages/404.jsx";
 
-const App = () => {
-  const router = createBrowserRouter({
-    routes: [
-      { path: "/", element: <Home /> },
-      { path: "/login", element: <Sigin /> },
-      { path: "/about", element: <About /> },
-      { path: "/dashboard", element: <Dashboard /> },
-      { path: "/dsource", element: <Dsource /> },
-      { path: "*", element: <Notfound /> },
-    ],
-  });
+function App() {
 
   return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-      <Routes router={router} />
-    </React.Suspense>
-  );
-};
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Home/>}/>
+        <Route path="/login" element={ <Sigin/>}/>
+        <Route path="/about" element={ <About/>}/>
+        <Route path="/dashboard" element={ <Dashboard/>}/>
+        <Route path="/dsource" element={ <Dsource/>}/>
+        <Route path="*" element={ <Notfound/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
+
+
 
 export default App;
 
